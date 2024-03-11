6d3881b57aec34cdbd9b8c8324a6f290344b1643
anewzhuang <anewzhuang@tencent.com>
2024-03-11 17:14:22 +0800

    feat(增加效果): 增加bloom效果+多pass实现

---
+++ assets/script/case_bloom.ts
@@ -0,0 +1,66 @@
+import RenderUtils from './render_utils';
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Case_Bloom extends cc.Component {
+
+    @property({ type: cc.Sprite, tooltip: CC_DEV && '精灵组件' })
+    protected sprite: cc.Sprite = null;
+
+    @property({ type: cc.Material, tooltip: CC_DEV && '渲染用的材质' })
+    protected material_highlight: cc.Material = null;
+
+    @property({ type: cc.Material, tooltip: CC_DEV && '渲染用的材质' })
+    protected material_gaussian: cc.Material = null;
+
+    @property({ type: cc.Material, tooltip: CC_DEV && '渲染用的材质' })
+    protected material_mix: cc.Material = null;
+
+    /**
+     * 正在使用的 RenderTexture
+     */
+    protected renderTexture: cc.RenderTexture = null;
+
+    /**
+     * 生命周期：开始（首次 update 前）
+     */
+    protected start() {
+        // 目标节点
+        const sprite = this.sprite;
+        const node = this.sprite.node;
+        // 设置材质
+        // 创建临时 RenderTexture
+        const srcRT = new cc.RenderTexture();
+        const dst1RT = new cc.RenderTexture();
+        const dst2RT = new cc.RenderTexture();
+        const finalRT = new cc.RenderTexture();
+        // 获取初始 RenderTexture
+        RenderUtils.getRenderTexture(node, srcRT);
+        // 多 Pass 处理
+        // 注：由于 OpenGL 中的纹理是倒置的，所以双数 Pass 的出的图像是颠倒的
+        
+        // 提取高亮部分
+        RenderUtils.renderWithMaterial(srcRT, dst1RT, this.material_highlight);
+        // 进行高斯模糊
+        RenderUtils.renderWithMaterial(dst1RT, dst2RT, this.material_gaussian);
+        // 进行混合
+        this.material_mix.setProperty('mixTexture', dst2RT);
+        RenderUtils.renderWithMaterial(srcRT, finalRT, this.material_mix);
+
+        // 使用经过处理的 RenderTexture
+        this.renderTexture = finalRT;
+        sprite.spriteFrame = new cc.SpriteFrame(this.renderTexture);
+        // 销毁不用的临时 RenderTexture
+        srcRT.destroy();
+        dst1RT.destroy();
+        dst2RT.destroy();
+    }
+
+    /**
+     * 生命周期：销毁
+     */
+    protected onDestroy(): void {
+        // 销毁不用的 RenderTexture
+        this.renderTexture && this.renderTexture.destroy();
+    }
+}
---
+++ assets/script/render_utils.ts
@@ -0,0 +1,70 @@
+export default class RenderUtil {
+    static getRenderTexture(node: cc.Node, out?: cc.RenderTexture) {
+        // 检查参数
+        if (!cc.isValid(node)) {
+            return null;
+        }
+        if (!out || !(out instanceof cc.RenderTexture)) {
+            out = new cc.RenderTexture();
+        }
+        // 获取宽高
+        const width = Math.floor(node.width),
+            height = Math.floor(node.height);
+        // 初始化 RenderTexture
+        out.initWithSize(width, height);
+        // 创建临时摄像机用于渲染目标节点
+        const cameraNode = new cc.Node();
+        cameraNode.parent = node;
+        const camera = cameraNode.addComponent(cc.Camera);
+        camera.clearFlags |= cc.Camera.ClearFlags.COLOR;
+        camera.backgroundColor = cc.color(0, 0, 0, 0);
+        camera.zoomRatio = cc.winSize.height / height;
+        // 将节点渲染到 RenderTexture 中
+        camera.targetTexture = out;
+        camera.render(node);
+        // 销毁临时对象
+        cameraNode.destroy();
+        // 返回 RenderTexture
+        return out;
+    }
+
+    static renderWithMaterial(srcRT: cc.RenderTexture, dstRT: cc.RenderTexture | cc.Material, material?: cc.Material) {
+        // 检查参数
+        if (dstRT instanceof cc.Material) {
+            material = dstRT;
+            dstRT = new cc.RenderTexture();
+        }
+        // 创建临时节点（用于渲染 RenderTexture）
+        const tempNode = new cc.Node();
+        tempNode.setParent(cc.Canvas.instance.node);
+        const tempSprite = tempNode.addComponent(cc.Sprite);
+        tempSprite.sizeMode = cc.Sprite.SizeMode.RAW;
+        tempSprite.trim = false;
+        tempSprite.spriteFrame = new cc.SpriteFrame(srcRT);
+        // 获取图像宽高
+        const width = srcRT.width,
+            height = srcRT.height;
+        // 初始化 RenderTexture
+        dstRT.initWithSize(width, height);
+        // 更新材质
+        if (material instanceof cc.Material) {
+            tempSprite.setMaterial(0, material);
+        }
+        // 创建临时摄像机（用于渲染临时节点）
+        const cameraNode = new cc.Node();
+        cameraNode.setParent(tempNode);
+        const camera = cameraNode.addComponent(cc.Camera);
+        camera.clearFlags |= cc.Camera.ClearFlags.COLOR;
+        camera.backgroundColor = cc.color(0, 0, 0, 0);
+        camera.zoomRatio = cc.winSize.height / height;
+        // 将临时节点渲染到 RenderTexture 中
+        camera.targetTexture = dstRT;
+        camera.render(tempNode);
+        // 销毁临时对象
+        cameraNode.destroy();
+        tempNode.destroy();
+        // 返回 RenderTexture
+        return dstRT;
+    }
+
+}
